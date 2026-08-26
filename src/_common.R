23c6671897f786f5b9c0280e33ce77e3d5af7602
fischerpj <fischer.pj@gmail.com>
2026-08-26 14:50:57 +0000

    2635

--- src/_common.R
+++ src/_common.R
@@ -18,9 +18,19 @@ big4_ <- function(x = db_()){
     dplyr::filter(stringr::str_detect(type, "^portal90"))
 }
 
+vitrine_ <- function(x = db_()){
+  x |>
+    dplyr::filter(stringr::str_detect(type, "^agentVitrine"))
+}
+
 spot_ <- function(x = db_()){
   x |>
     dplyr::filter(stringr::str_detect(type, "^spot"))
+}
+
+locals_ <- function(x = db_()){
+  x |>
+    dplyr::filter(stringr::str_detect(type, "^agent_lind"))
 }
 
 db_ <- function() {
@@ -36,10 +46,32 @@ db_ <- function() {
     "lystio",   11,  "portal_active",NA,
     "K&P immo",   3, "agentTop",NA,
     "Remax NPK",   3, "agentTop",NA,
+    "immoi",   3, "agentVitrine","https://www.immoi.at/",
+    "marktluecke",   3, "agentVitrine","https://www.marktluecke.at/",
+    "Raiffeisen Kärnten",   3, "agentVitrine","https://www.raiffeisen-immobilien.at/de",
+    "Remax-idea",   3, "agentVitrine","https://www.remax.at/de/ib/remax-idea-villach",
+    "volksbank-kaernten",   3, "agentVitrine","https://www.volksbank-kaernten.at/immobilien/immobilien_startseite.page",
+    "fischer-immobilien",   3, "agentVitrine","https://www.fischer-immobilien.at/",
+    "topmiete",   3, "agentVitrine","https://topmiete.at/",
+    "nageler",   3, "agentVitrine","https://nageler.biz/",
+    "markerimmobilien",   3, "agentVitrine","https://www.mi.at/",
+    "perkonig",   3, "agentVitrine","http://www.realitaeten-perkonig.at",
+    "immobilien-at",   3, "agentVitrine","https://www.immobilien-at.com/immobilienmakler/?ma9037-realitaeten_perkonig",
+    "immobilienmarkt",   3, "agentVitrine","https://www.a-immobilienmarkt.at/immobilienmakler/immobilien/?ma9037-realitaeten_perkonig",
+    "schiffer",   3, "agentVitrine","https://www.schiffer-immobilien.at/immobilien",
+    "sp-immo",   3, "agentVitrine","https://sp-immo.at/",
+    "adunka",   3, "agentVitrine","https://www.adunka.at/leistungen/",
+    "wallner",   3, "agentVitrine","https://wallner-immobilien.at/",
+    "remax-friends",   3, "agentVitrine","http://www.remax-friends.at",
+    "deinzuhauseimmobilien",   3, "agentVitrine","https://www.deinzuhauseimmobilien.com/",
+    "willi mann",   3, "agentVitrine","https://www.wmi.at",
+    "blattgold",   3, "agentVitrine","https://anleihe.kollitsch.eu/projekt-blattgold-in-klagenfurt/",
+    "sreal",   3, "agentVitrine","https://www.sreal.at/de/home",
+    "patronum",   3, "agentVitrine","https://patronum.at/",
+    "raireal",   3, "agentVitrine",NA,
     "FullHouseImmo",   3, "agent",NA,
     "Golden immo",   3, "agent",NA,
     "sREAL",   3, "agent",NA,
-    "Raiffeisen",   3, "agent",NA,
     "Kompagnon",   3, "agent",NA,
     "Immo Consulting",   3, "agent",NA,
     "Schick",   3, "agent",NA,
@@ -71,7 +103,8 @@ db_ <- function() {
     "cozycozy",   3, NA,NA,
     "vacationRenter",   3, NA, NA,
     "Landskron|Treffen|Warmbad|St.Magdalen",   3, "spot_vil", NA,
-    "Annabichl|Waidmannsdorf|St.Peter|Viktring",   3, "spot_kla",NA
+    "Annabichl|Waidmannsdorf|St.Peter|Viktring",   3, "spot_kla",NA,
+    "ERA Immobilien Villach", 3,"agent_lind", "https://era.at/de/",
   ) |> data.table::as.data.table()
   return(data)
 }
